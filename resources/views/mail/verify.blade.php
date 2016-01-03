67ffdaf8319b815e4ec30200cd29a9bd44aeb79d
samundrak-amazon <samundrak@yahoo.com>
2016-01-03 06:51:58 +0000

    From remote

--- resources/views/mail/verify.blade.php
+++ resources/views/mail/verify.blade.php
@@ -1,2 +1,2 @@
 Please click below link to Verify your Account
-<a href="http://localhost:3000/verify/{{$hash}}" target="_blank">Click Here</a>
+<a href="http://52.33.26.178/verify/{{$hash}}" target="_blank">Click Here</a>
--- resources/views/mail/view.blade.php
+++ resources/views/mail/view.blade.php
@@ -1,2 +1,2 @@
 Please click below link to reset your password
-<a href="http://localhost:3000/reset_password/{{$hash}}" target="_blank">Click Here</a>
+<a href="http://52.33.26.178/reset_password/{{$hash}}" target="_blank">Click Here</a>
